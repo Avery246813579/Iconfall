1b3772966312a5e815303eb81ce54080e02b853a
Avery Durrant <avery246813579@gmail.com>
2016-09-27 12:45:02 -0400

    Added template Vector and Location files

---
+++ src/Location.js
@@ -0,0 +1,13 @@
+if(typeof window.iconfall == "undefined"){
+    window.iconfall = {};
+}
+
+function Location(){
+    var self = this;
+
+    (function Constructor(){
+
+    }).apply(self, arguments);
+}
+
+iconfall.Location = Location;
---
+++ src/Vector.js
@@ -0,0 +1,13 @@
+if (typeof window.iconfall == "undefined") {
+    window.iconfall = {};
+}
+
+function Vector() {
+    var self = this;
+
+    (function Constructor(x, y, t) {
+
+    }).apply(self, arguments);
+}
+
+iconfall.Vector = Vector;
